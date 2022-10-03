ce2672993915f33c2188bdc0b1438d564deb4fb5
sa96125 <sa96125@gmail.com>
2022-10-03 14:25:05 +0900

    solved(22/10/03)

--- programmers.playground/Contents.swift
+++ programmers.playground/Contents.swift
@@ -1,1 +1,21 @@
+import Foundation
 
+
+let test = StageOne()
+
+
+test.isEvenOrOdd(3)
+test.isEvenOrOdd2(4)
+test.getAverageOfArray([1,2,3,4])
+test.getSumOfFactors(12)
+test.getSumOfDigits(987)
+test.addOneIfSquareRootOfInteger(121)
+
+
+test.flipNumbersToArray(12345)
+test.AreNumbersOfPAndYEqual("pPoooyY")
+test.AreNumbersOfPAndYEqual2("Pyy")
+test.isHarshadNumber(12)
+test.convertToInteger("-1234")
+test.descend(118372)
+    
--- programmers.playground/Sources/StageOne.swift
+++ programmers.playground/Sources/StageOne.swift
@@ -1,16 +1,17 @@
 import Foundation
 
 
-struct StageOne {
+public struct StageOne {
     
+    public init() {}
     
     // MARK:  Odd and Even
     
-    func isEvenOrOdd(_ num:Int) -> String {
+    public func isEvenOrOdd(_ num:Int) -> String {
         return num % 2 == 0 ? "Even" : "Odd"
     }
     
-    func isEvenOrOdd2(_ num:Double) -> String {
+    public func isEvenOrOdd2(_ num:Double) -> String {
         return num.remainder(dividingBy: 2) == 0 ? "Even" : "Odd"
     }
     
@@ -19,7 +20,7 @@ struct StageOne {
     
     // MARK:  Average
     
-    func getAverageOfArray(_ arr:[Int]) -> Double {
+    public func getAverageOfArray(_ arr:[Int]) -> Double {
         return Double(arr.reduce(0, +)) / Double(arr.count)
     }
     
@@ -28,7 +29,7 @@ struct StageOne {
     
     // MARK:  Sum of factors
     
-    func getSumOfFactors(_ num:Int) -> Int {
+    public func getSumOfFactors(_ num:Int) -> Int {
         guard num != 0 else { return 0 }
             
         return Array(1...num).filter{ num % $0 == 0 }.reduce(0, +)
@@ -39,7 +40,7 @@ struct StageOne {
     
     // MARK:  Add digits
     
-    func getSumOfDigits(_ n:Int) -> Int {
+    public func getSumOfDigits(_ n:Int) -> Int {
         return String(n).reduce(0){ $0 + Int(String($1))! }
     }
 
@@ -48,10 +49,77 @@ struct StageOne {
     
     // MARK:  Determine the square root of an integer
     
-    func getSumOfDigits2(_ n:Int64) -> Int64 {
+    public func addOneIfSquareRootOfInteger(_ n:Int64) -> Int64 {
         let sqrtedNumber = sqrt(Double(n))
         
         return sqrtedNumber.truncatingRemainder(dividingBy: 1.0) == 0 ? Int64(pow(sqrtedNumber + 1, 2)) : -1
     }
 
+    
+    
+    
+    // MARK: Flip natural numbers to an array
+    
+    public func flipNumbersToArray(_ n:Int64) -> [Int] {
+        return String(n).compactMap{Int(String($0))}.reversed()
+    }
+    
+    
+    
+    
+    // MARK: Number of p and y in string
+    
+    public func AreNumbersOfPAndYEqual(_ s:String) -> Bool {
+        let string = s.lowercased()
+        var count = 0
+
+        for c in string {
+            
+            if c == "p" {
+                count += 1
+                continue
+            }
+            
+            if c == "y" {
+                count -= 1
+                continue
+            }
+        }
+
+        return count == 0 ? true : false
+    }
+    
+    public func AreNumbersOfPAndYEqual2(_ s:String) -> Bool {
+        let string = s.lowercased()
+        
+        return string.components(separatedBy: "p").count == string.components(separatedBy: "y").count
+    }
+    
+    
+    
+    
+    // MARK: Harshad number
+    
+    public func isHarshadNumber(_ x:Int) -> Bool {
+        return x % String(x).reduce(0){ $0 + Int(String($1))! } == 0
+    }
+    
+    
+    
+    
+    // MARK: Convert string to integer
+    
+    public func convertToInteger(_ s:String) -> Int {
+        return Int(s)!
+    }
+    
+    
+    
+    
+    // MARK: Placing Integers in Descending Order
+    
+    public func descend(_ n:Int64) -> Int64 {
+        return Int64(String(String(n).sorted{ $0 > $1 }))!
+    }
+    
 }
